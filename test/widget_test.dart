31222e008f6c86fe7998ccc62f27347933291f12
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-02-02 16:56:19 +0700

    display pwa fullscreen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_slides_portrait/main.dart';
+import 'package:mschool_ecommerce/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
