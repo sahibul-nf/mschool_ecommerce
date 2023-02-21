37b29f36de33de9a5b842261f22494b4c44c02c3
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-02-22 01:32:37 +0700

    tes

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   SystemChrome.setEnabledSystemUIMode(
-    SystemUiMode.leanBack,
+    SystemUiMode.immersiveSticky,
   );
 
   runApp(
