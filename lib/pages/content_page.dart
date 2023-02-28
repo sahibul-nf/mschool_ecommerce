c6e24205806dcd5540154e99cf10e8cd7e272166
sahibul_nf <sahibulnuzulfirdaus13@gmail.com>
2023-03-01 03:40:36 +0700

    🎈 perf: optimize white space

--- lib/pages/content_page.dart
+++ lib/pages/content_page.dart
@@ -1,9 +1,12 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mschool_ecommerce/models/content.dart';
 import 'package:mschool_ecommerce/pages/content_detail_page.dart';
 import 'package:mschool_ecommerce/providers/dark_mode_provider.dart';
+import 'package:mschool_ecommerce/themes/text.dart';
+import 'package:mschool_ecommerce/themes/utils.dart';
 import 'package:mschool_ecommerce/widgets/markdown_widget.dart';
 
 class ContentPage extends ConsumerWidget {
@@ -21,106 +24,118 @@ class ContentPage extends ConsumerWidget {
         MediaQuery.of(context).orientation == Orientation.portrait;
     final isDarkMode = ref.watch(darkModeProvider);
 
-    return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: (isTour) ? 0 : 20,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              const SizedBox(height: 20),
-              Image.asset(
-                isDarkMode ? content.imageDark! : content.image!,
-                fit: BoxFit.scaleDown,
-                height: MediaQuery.of(context).size.height *
-                    (isPortrait ? 0.35 : 0.5),
-              ),
-              const SizedBox(height: 20),
-              FittedBox(
-                fit: BoxFit.scaleDown,
-                child: SelectableText.rich(
-                  TextSpan(
-                    text: content.title!,
-                    style: GoogleFonts.roboto(
-                      textStyle: TextStyle(
-                        fontFamily: "RobotoSerif",
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColor,
-                        height: 1.3,
-                        fontSize: 20,
+    return SafeArea(
+      bottom: !isTour,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  Image.asset(
+                    isDarkMode ? content.imageDark! : content.image!,
+                    fit: BoxFit.scaleDown,
+                    height: MediaQuery.of(context).size.height *
+                        (isPortrait
+                            ? isTour
+                                ? 0.4
+                                : 0.35
+                            : 0.5),
+                  ),
+                  const SizedBox(height: 20),
+                  FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: AutoSizeText.rich(
+                      TextSpan(
+                        text: content.title!,
+                        style: pStyle(
+                          context,
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                          height: 1.3,
+                          fontSize: smallScreen(context) ? 20 : 22,
+                        ),
+                        // style: GoogleFonts.roboto(
+                        //   textStyle: TextStyle(
+                        //     fontFamily: "RobotoSerif",
+                        //     fontWeight: FontWeight.bold,
+                        //     color: Theme.of(context).primaryColor,
+                        //     height: 1.3,
+                        //     fontSize: 22,
+                        //   ),
+                        // ),
                       ),
+                      textAlign: TextAlign.center,
                     ),
                   ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              const SizedBox(height: 20),
-              if (content.subtitle!.isNotEmpty)
-                Row(
-                  children: [
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: SelectableText.rich(
-                        TextSpan(
-                          text: content.subtitle!,
-                          style: GoogleFonts.roboto(
-                            textStyle: const TextStyle(
-                              fontFamily: "RobotoSerif",
-                              fontWeight: FontWeight.bold,
-                              height: 1.3,
-                              color: Color(0xffc16464),
-                              fontSize: 18,
+                  const SizedBox(height: 10),
+                  if (content.subtitle!.isNotEmpty)
+                    Row(
+                      children: [
+                        FittedBox(
+                          fit: BoxFit.scaleDown,
+                          child: AutoSizeText.rich(
+                            TextSpan(
+                              text: content.subtitle!,
+                              style: pStyle(
+                                context,
+                                fontWeight: FontWeight.bold,
+                                color: const Color(0xffc16464),
+                                height: 1.3,
+                                fontSize: smallScreen(context) ? 18 : 20,
+                              ),
                             ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  if (content.subtitle!.isNotEmpty) const SizedBox(height: 8),
+                  MyMarkdownWidget(content.shortDescription!),
+                ],
+              ),
+            ),
+            // if (!isTour) const Spacer(),
+            // read more button
+            if (!isTour)
+              Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (!isTour) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ContentDetailPage(content),
+                          ),
+                        );
+                      }
+                    },
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        "Read more",
+                        style: GoogleFonts.roboto(
+                          textStyle: const TextStyle(
+                            fontFamily: "RobotoSerif",
+                            fontSize: 16,
                           ),
                         ),
                       ),
                     ),
-                  ],
-                ),
-              if (content.subtitle!.isNotEmpty) const SizedBox(height: 10),
-              MyMarkdownWidget(content.shortDescription!),
-            ],
-          ),
-
-          if (!isTour) const SizedBox(height: 10),
-          // read more button
-          if (!isTour)
-            ElevatedButton(
-              onPressed: () {
-                if (!isTour) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ContentDetailPage(content),
-                    ),
-                  );
-                }
-              },
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Text(
-                  "Read more",
-                  style: GoogleFonts.roboto(
-                    textStyle: const TextStyle(
-                      fontFamily: "RobotoSerif",
-                      fontSize: 16,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      elevation: 0,
+                      padding: const EdgeInsets.symmetric(horizontal: 24),
                     ),
                   ),
-                ),
+                ],
               ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                elevation: 0,
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-              ),
-            )
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/pages/tutorial_page.dart
+++ lib/pages/tutorial_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mschool_ecommerce/providers/tutorial_provider.dart' as tp;
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class TutorialPage extends ConsumerStatefulWidget {
   const TutorialPage({
@@ -45,38 +44,38 @@ class _TutorialPageState extends ConsumerState<TutorialPage> {
         ),
 
         // skip button and smooth page indicator
-        Container(
-          height: 45,
-          color: Colors.grey[200],
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextButton(
-                onPressed: () {
-                  tutorialProvider.setFirstTime(false);
-                },
-                child: const Text("Skip"),
-              ),
-              SmoothPageIndicator(
-                controller: tutorialProvider.pageController,
-                count: tutorialProvider.pages.length,
-                textDirection: TextDirection.rtl,
-                axisDirection: Axis.horizontal,
-                onDotClicked: (index) {
-                  tutorialProvider.setCurrentPage(index);
-                },
-                effect: ExpandingDotsEffect(
-                  dotHeight: 8,
-                  dotWidth: 8,
-                  dotColor: Colors.grey.shade400,
-                  activeDotColor: Colors.blue,
-                  spacing: 5,
-                ),
-              ),
-            ],
-          ),
-        )
+        // Container(
+        //   height: 45,
+        //   color: Colors.grey[200],
+        //   padding: const EdgeInsets.symmetric(horizontal: 20),
+        //   child: Row(
+        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //     children: [
+        //       TextButton(
+        //         onPressed: () {
+        //           tutorialProvider.setFirstTime(false);
+        //         },
+        //         child: const Text("Skip"),
+        //       ),
+        //       SmoothPageIndicator(
+        //         controller: tutorialProvider.pageController,
+        //         count: tutorialProvider.pages.length,
+        //         textDirection: TextDirection.rtl,
+        //         axisDirection: Axis.horizontal,
+        //         onDotClicked: (index) {
+        //           tutorialProvider.setCurrentPage(index);
+        //         },
+        //         effect: ExpandingDotsEffect(
+        //           dotHeight: 8,
+        //           dotWidth: 8,
+        //           dotColor: Colors.grey.shade400,
+        //           activeDotColor: Colors.blue,
+        //           spacing: 5,
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // )
       ],
     );
   }
